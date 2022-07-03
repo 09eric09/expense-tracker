9cf5c69ef8d039135e495284826fdbe442ba4140
09eric09 <09eric09@gmail.com>
2022-07-02 23:32:17 -0700

    created the ExpenseDate component to get dates & passed the dat props from App.js to ExpenseItem to ExpenseDate.js (three passes deep)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const year = props.date.getFullYear();
 
   return (
   <div className="expense-item">
-    <div>{month}</div>
-    <div>{year}</div>
-    <div>{day}</div>
+    <ExpenseDate date={props.date}/>
     <div className="expense-item__description">
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
@@ -20,4 +16,4 @@ const ExpenseItem = (props) => {
   )
 }
 
-export default ExpenseItem
+export default ExpenseItem;
