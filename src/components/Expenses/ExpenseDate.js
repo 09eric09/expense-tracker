f13bd5acd01aad097eaf67145bc7cfefbd671132
09eric09 <09eric09@gmail.com>
2022-07-04 17:00:21 -0700

    updated directory paths for better organization

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Expenses/ExpenseDate.css';
+
+const ExpenseDate = (props) => {
+    //These are the props from the App.js file, passed from ExpenseItem.js
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+    <div className="expense-date__month">{month}</div>
+    <div className="expense-date__year">{year}</div>
+    <div className="expense-date__day">{day}</div>
+    </div>
+  )
+}
+
+export default ExpenseDate;
