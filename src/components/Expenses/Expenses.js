108a1223007389c391ac486e86412e9d194fd64e
09eric09 <09eric09@gmail.com>
2022-07-06 22:55:18 -0700

    passed selected date value from ExpenseFilter up to Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('');
+
+  const saveDateHandler = (selectedYear) => {
+    console.log('Expenses.js');
+    console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  }
+
   return (
+    <div>
     <Card className="expenses">
+    <ExpensesFilter onChangeFilter={saveDateHandler} selected={filteredYear} />
       <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
       <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
       <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
       <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>       
     </Card>
+    </div>
   )
 }
 
