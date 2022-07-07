3122185b02f7c320c5216b4912ee02793e1c7dc3
09eric09 <09eric09@gmail.com>
2022-07-07 10:40:21 -0700

    [64] mapped through each ExpenseItem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,8 +8,6 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('');
 
   const saveDateHandler = (selectedYear) => {
-    console.log('Expenses.js');
-    console.log(selectedYear);
     setFilteredYear(selectedYear);
   }
 
@@ -17,10 +15,9 @@ const Expenses = (props) => {
     <div>
     <Card className="expenses">
     <ExpensesFilter onChangeFilter={saveDateHandler} selected={filteredYear} />
-      <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
-      <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-      <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
-      <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>       
+    {props.expenses.map(expense => (
+      <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>  
+    ))}  
     </Card>
     </div>
   )
