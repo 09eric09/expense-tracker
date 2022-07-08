114148072d9508cc02d505cbd74fb4ddfc358a3a
09eric09 <09eric09@gmail.com>
2022-07-07 21:44:59 -0700

    cleanup

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('');
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const saveDateHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
