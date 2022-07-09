9ee8f5bbc0fb3c8a77e8b101e3780c5ac2812f68
09eric09 <09eric09@gmail.com>
2022-07-08 22:18:33 -0700

    cleanup to ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,22 +4,14 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  //First arguement is a refercence to props.title, which is the initial value
-  //Second argument is the fucntion we call to update the state
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated via useState Hook !!!');
-  }
 
   return (
   <Card className="expense-item">
     <ExpenseDate date={props.date}/>
     <div className="expense-item__description">
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
     </div>
-    <button onClick={clickHandler}>Change Title</button>
   </Card>
   )
 }
